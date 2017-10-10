dc94fb36dbe1a5591afa9526d56e2e6823bd16fa
Vaishant Kameswaran <vaishantkameswaran@gmail.com>
2017-10-10 15:16:29 +0800

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Vaishant Kameswaran on 10/10/17.
+//  Copyright © 2017 VaiRox. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
