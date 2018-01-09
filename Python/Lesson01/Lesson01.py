0b8e814550216bb844cf6622e7062c0740e1c4c9
Vaishant Kameswaran <vaishantkameswaran@gmail.com>
2018-01-09 22:17:14 +0800

    Added Python folder
    Added the details for lesson 1 of our Python class.

---
+++ Python/Lesson01/Lesson01.py
@@ -0,0 +1,32 @@
+# Printing
+print("Hello world")
+
+# Variables
+a = 3
+
+# Printing variables
+print("The value of a is", a)
+
+# User input
+name = input("What is your name? ")
+print("Hello", name)
+
+# Conditional statements
+if name == "Bob":
+	print("Your name is Bob")
+elif name == "John":
+	print("Your name is John")
+else:
+	print("Your name is not Bob or John")
+
+print("This will always print")
+
+# Lists
+alphaList = ["a", "b", "c", "d"]
+alphaList.append("e")
+
+# Dictionaries
+alphaDict = {"a":1, "b":2, "c":3}
+print(alphaList[0])
+
+
